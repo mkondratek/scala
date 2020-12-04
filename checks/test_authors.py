c67c97e62e2ab0ed5df12e01b30ea5f41d07ca23
mkondratek <mkondratek@virtuslab.com>
2020-12-04 13:48:27 +0100

    Move topics from /topics to /courses/scala/topics

--- checks/test_authors.py
+++ checks/test_authors.py
@@ -3,7 +3,8 @@ from structure_loader import load_structure
 STRUCTURE = load_structure()
 
 def test_author_exists():
-  author_ids = list(map(lambda author : author.id, STRUCTURE.authors))
-  for topic in STRUCTURE.topics:
-    for lesson in topic.lessons:
-      assert lesson.author_id in author_ids, f"Lesson {lesson.topic_id}/{lesson.id} author {lesson.author_id} is not included in authors.json file"
+  author_ids = list(map(lambda author: author.id, STRUCTURE.authors))
+  for course in STRUCTURE.courses:
+    for topic in STRUCTURE.topics[course.id]:
+      for lesson in topic.lessons:
+        assert lesson.author_id in author_ids, f"Lesson {course.id}/{lesson.topic_id}/{lesson.id} author {lesson.author_id} is not included in authors.json file"
